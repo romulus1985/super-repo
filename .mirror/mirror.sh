e1aad00b61be0d54dc08c578fa00b87e01128d71
Romulus <287306752@qq.com>
2020-05-10 14:11:54 +0800

    Improve the file format.

--- .mirror/mirror.sh
+++ .mirror/mirror.sh
@@ -6,7 +6,7 @@
 mirror_config=".mirror/$SUPER_REPO_MIRROR".conf
 # echo "mirror_config = $mirror_config"
 
-if [ -f  "$mirror_config" ]; then
+if [ -f "$mirror_config" ]; then
     used_config=$mirror_config
 else
     used_config=.mirror/tsinghua.conf
--- super_repo.sh
+++ super_repo.sh
@@ -27,19 +27,19 @@ function isNetworkConnected() {
 }
 
 function getRepoInitUrl() {
-#   echo "fuc getRepoInitUrl enter"
+   # echo "fuc getRepoInitUrl enter"
    repo_init_url="./repo init -u $SUPER_MIRROR_MANI_URL  $SUPER_REPO_INIT_ARGS"
    echo "$repo_init_url"
 }
 
 function createRepo() {
-    if [ ! -f repo ]; then
-       echo "download repo..."
-       downloadRepo
-    else
-       echo "repo already existed."
-    fi
-    chmod +x repo
+   if [ ! -f repo ]; then
+      echo "download repo..."
+      downloadRepo
+   else
+      echo "repo already existed."
+   fi
+   chmod +x repo
 }
 
 function getSyncMode() {
@@ -47,15 +47,13 @@ function getSyncMode() {
    #echo "SUPER_REPO_PRE_FAST_JOS_TIMES = $SUPER_REPO_PRE_FAST_JOS_TIMES"
    mycount=$1
    # if [ "$mycount" -gt 10 ]
-   if [ "$mycount" -gt "$SUPER_REPO_PRE_FAST_JOS_TIMES" ]
-   then
+   if [ "$mycount" -gt "$SUPER_REPO_PRE_FAST_JOS_TIMES" ]; then
       ret=$(expr $mycount % 2)
-      if [ 0 == "$ret" ] 
-      then
+      if [ 0 == "$ret" ]; then
          return 0
       else
          return 1
-      fi 
+      fi
    else
       return 1
    fi
@@ -68,10 +66,9 @@ function setWindowScaling() {
 
 function runRepoSync() {
    sleep 3
-   count=$(($count+1))
+   count=$(($count + 1))
    getSyncMode "$count"
-   if [ 0 == $? ] 
-   then
+   if [ 0 == $? ]; then
       # proxy mode
       setWindowScaling 0
       j_ops=" -j1 "
@@ -92,12 +89,12 @@ function setRepoUrl() {
    export REPO_URL=$SUPER_MIRROR_REPO_URL
 }
 
-function forceSetMirrorUrl(){
-  # force replate url in .repo/manifests/
-  echo "force set mirror url to manifest"
-  # replace https://android.googlesource.com with mirror host 
-  # For example: https://aosp.tuna.tsinghua.edu.cn
-  find .repo/manifests/ -name *.xml | xargs sed -i -e "s#\(fetch=\"\)https://android.googlesource.com/\"#\1$SUPRE_MIRROR_HOST\"#g"
+function forceSetMirrorUrl() {
+   # force replate url in .repo/manifests/
+   echo "force set mirror url to manifest"
+   # replace https://android.googlesource.com with mirror host
+   # For example: https://aosp.tuna.tsinghua.edu.cn
+   find .repo/manifests/ -name *.xml | xargs sed -i -e "s#\(fetch=\"\)https://android.googlesource.com/\"#\1$SUPRE_MIRROR_HOST\"#g"
 
 }
 
@@ -106,8 +103,8 @@ function getPythonVersion() {
 }
 
 isNetworkConnected
-if [ $? != 0 ]; then 
-    exit 1
+if [ $? != 0 ]; then
+   exit 1
 fi
 
 createRepo
@@ -123,8 +120,7 @@ ${repo_init_url}
 forceSetMirrorUrl
 
 runRepoSync
-while [ $? -ne 0 ]
-do
+while [ $? -ne 0 ]; do
    # clean temp file created when download failed.
    echo "Download failed. Clean temp files in .repo/project-objects/"
    find .repo/project-objects/ -name "tmp_*" | xargs rm -rf
